62184dd74e484fd6a9374877524333082a0b3639
Vinh Do <vinhdo07@gmail.com>
2019-02-26 21:37:34 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,42 @@
+require 'pry'
+
 class Owner
-  # code goes here
+  attr_accessor :name, :pets
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+    @pets = {fishes: [], dogs: [] , cats: []}
+  end
+
+  def species
+    @species = "human"
+  end
+
+  def say_species
+    "I am a human."
+  end
+
+  def pets
+    @pets
+  end
+
+  def buy_fish(name)
+    fish = Fish.new(name)
+    self.pets[:fishes] << fish
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    @@all.size
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
 end
